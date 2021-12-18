8647bfe92f7ed7442486397f4a4793b3e5154b2f
이용광 <sksda4614@naver.com>
2021-12-18 22:31:14 +0900

    loginServlet, dbCon

---
+++ Second-hand_web/src/mybean/db/dbCon.java
@@ -0,0 +1,23 @@
+package mybean.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class dbCon {
+	public static Connection getConnection() throws ClassNotFoundException, SQLException {
+		Connection conn = null;
+		
+		String jdbcDriver = "com.mysql.cj.jdbc.Driver";
+		String jdbcUrl = "jdbc:mysql://localhost:3306/empdb?serverTimezone=UTC";
+		String dbUser = "root";
+		String dbPwd = "12341234";
+		
+		Class.forName(jdbcDriver);
+		// 2. Database Connection 연결
+		conn = DriverManager.getConnection(jdbcUrl, dbUser, dbPwd);
+		
+		return conn;
+	}
+}
+
