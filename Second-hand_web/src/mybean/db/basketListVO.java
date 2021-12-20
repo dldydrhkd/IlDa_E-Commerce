2f425e7b11a2554fea66368e8b87fafcefb5ab5c
이용광 <sksda4614@naver.com>
2021-12-20 09:54:50 +0900

    basketDAO, basketListVO
    넘겨줄 객체인 baskitListVO와 basketDAO list 구현

---
+++ Second-hand_web/src/mybean/db/basketListVO.java
@@ -0,0 +1,41 @@
+package mybean.db;
+
+public class basketListVO {
+	private int userNumber;
+	private int basketNumber;
+	private String noticeTitle;
+	
+	public basketListVO(int userNumber, int basketNumber, String noticeTitle) {
+		super();
+		this.userNumber = userNumber;
+		this.basketNumber = basketNumber;
+		this.noticeTitle = noticeTitle;
+	}
+
+	public int getUserNumber() {
+		return userNumber;
+	}
+
+	public void setUserNumber(int userNumber) {
+		this.userNumber = userNumber;
+	}
+
+	public int getBasketNumber() {
+		return basketNumber;
+	}
+
+	public void setBasketNumber(int basketNumber) {
+		this.basketNumber = basketNumber;
+	}
+
+	public String getNoticeTitle() {
+		return noticeTitle;
+	}
+
+	public void setNoticeTitle(String noticeTitle) {
+		this.noticeTitle = noticeTitle;
+	}
+	
+	
+	
+}
