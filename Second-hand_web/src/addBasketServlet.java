ab1c134aa3bcd31cc6c76201bb4901703c9e689e
이용광 <sksda4614@naver.com>
2021-12-20 17:28:52 +0900

    addBasketServlet
    찜 추가

---
+++ Second-hand_web/src/addBasketServlet.java
@@ -0,0 +1,47 @@
+
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import mybean.db.basketDAO;
+import mybean.db.basketVO;
+
+@WebServlet("/addBasketServlet")
+public class addBasketServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public addBasketServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		HttpSession session = request.getSession();
+		
+		int basketNumber = Integer.parseInt(request.getParameter("basketNumber"));
+		int userNumber = (int) session.getAttribute("userNumber");
+		int noticeNumber = Integer.parseInt(request.getParameter("noticeNumber"));
+		
+		try {
+			basketDAO basket = basketDAO.getInstance();
+			basket.insertBasket(new basketVO(basketNumber, userNumber, noticeNumber));
+			basket.disConnect();
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
