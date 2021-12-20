f0d9bf6e4bcdfb8c4a4a69c9d5d8dbe0d0887c1e
Geonwoo <kkw2048@daum.net>
2021-12-20 09:24:30 +0900

    commentDAO, commentVO

---
+++ Second-hand_web/src/mybean/db/commentDAO.java
@@ -0,0 +1,93 @@
+package mybean.db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class commentDAO {
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+
+	// Singleton
+	private static commentDAO comment;
+
+	private commentDAO() throws SQLException, ClassNotFoundException {
+		conn = dbCon.getConnection();
+	}
+
+	public static commentDAO getInstance() throws SQLException, ClassNotFoundException {
+		if(comment==null) {
+			comment = new commentDAO();
+		}
+		return comment;
+	}
+	
+	
+	public void insertRecord(commentVO comment) throws SQLException {
+		String sql = "insert into commentTbl(commentNumber, commentInfo, noticeNumber, userId) "
+				+ " values(?,?,?,?)";
+		
+		pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1,  null);
+		pstmt.setString(2,  comment.getCommentInfo());
+		pstmt.setInt(3,  comment.getNoticeNumber());
+		pstmt.setString(4,  comment.getUserId());
+		
+		pstmt.executeUpdate();
+	}
+	
+	public void updateRecord(commentVO comment) throws SQLException {
+		String sql = "update commentTbl set commentInfo=? "
+				+ "where commentNumber=? and noticeNumber=? and userId= ? ";
+		pstmt = conn.prepareStatement(sql);
+		
+		pstmt.setString(1,  comment.getCommentInfo());
+		pstmt.setInt(2,  comment.getCommentNumber());
+		pstmt.setInt(3,  comment.getNoticeNumber());
+		pstmt.setString(4,  comment.getUserId());
+		
+		pstmt.executeUpdate();
+	}
+	
+	public void deleteRecord(commentVO comment) throws SQLException {
+		String sql = "delete from noticeTbl "
+				+ "where commentNumber=? and noticeNumber=? and userId= ? ";
+		pstmt = conn.prepareStatement(sql);
+		
+		pstmt.setInt(1, comment.getCommentNumber());
+		pstmt.setInt(2,  comment.getNoticeNumber());
+		pstmt.setString(3,  comment.getUserId());
+		
+		pstmt.executeUpdate();
+	}
+	
+	public List<commentVO> listRecord(commentVO comment) throws SQLException {
+		
+		List<commentVO> commentList = new ArrayList<commentVO>();
+		
+		String sql = "select * from commentTbl where noticeNumber=?";
+			
+		pstmt = conn.prepareStatement(sql);
+		pstmt.setInt(1, comment.getNoticeNumber());
+			
+		rs = pstmt.executeQuery();
+			
+		while(rs.next()) {	
+			commentList.add(new commentVO(rs.getString("userId"), rs.getString("commentInfo")));
+		}	
+		return commentList;
+		 
+	}
+	
+	public void disConnect() throws SQLException {
+		if(rs != null) rs.close();
+		if(rs != null) pstmt.close();
+		if(rs != null) conn.close();
+	}
+
+}
