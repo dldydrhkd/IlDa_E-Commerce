a94f9c8c2f66079f5ddd385ef1c5bdae9393e2f3
이용광 <sksda4614@naver.com>
2021-12-20 11:50:03 +0900

    writeServlet
    게시글 생성 서블릿

--- Second-hand_web/src/writeServlet.java
+++ Second-hand_web/src/writeServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import mybean.db.noticeDAO;
 import mybean.db.noticeVO;
@@ -24,7 +25,25 @@ public class writeServlet extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		String title = request.getParameter("title");
+		String info = request.getParameter("info");
+		String state = request.getParameter("state");
+		String classification = request.getParameter("classification");
+		HttpSession session = request.getSession();
+		int userNumber = (int) session.getAttribute("userNumber");
 		
+		noticeVO n = new noticeVO(title, info, state, classification, userNumber);
+		
+		try {
+			noticeDAO notice = noticeDAO.getInstance();
+			notice.insertRecord(n);
+			notice.disConnect();
+			response.sendRedirect("main.jsp");
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
