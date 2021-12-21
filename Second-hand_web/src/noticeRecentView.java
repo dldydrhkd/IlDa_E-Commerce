636d87f3c3c79452b7686655508fb4246d4abe91
이용광 <sksda4614@naver.com>
2021-12-22 00:10:00 +0900

    noticeDAO, noticeRecentView
    noticeDAO 최신 결과 가져오기 구현, noticeRecentView 서블릿 작성

---
+++ Second-hand_web/src/noticeRecentView.java
@@ -0,0 +1,46 @@
+
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import mybean.db.noticeDAO;
+import mybean.db.noticeVO;
+
+
+@WebServlet("/noticeFindView")
+public class noticeRecentView extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+    public noticeRecentView() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		HttpSession session = request.getSession();
+		int userNumber = (int) session.getAttribute("userNumber");
+		try {
+			noticeDAO notice = noticeDAO.getInstance();
+			noticeVO n = notice.getRecentNotice(userNumber);
+			notice.disConnect();
+			RequestDispatcher rd = request.getRequestDispatcher("noticeView.jsp");
+			request.setAttribute("notice", n);
+			rd.forward(request, response);
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
